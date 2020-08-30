9cfd284ab5dd24a073ae88efb35a7321355f34e2
Chase <chaseconaway82@gmail.com>
2020-08-30 15:26:00 -0700

    finished up RoomCarpet and RoomDimension

--- src/RoomCarpet.java
+++ src/RoomCarpet.java
@@ -1,6 +1,22 @@
 public class RoomCarpet {
 	
+	public RoomDimension roomD;
+	double cost;
 	
+	public RoomCarpet(RoomDimension roomD, double cost) {
+		this.roomD = new RoomDimension(roomD.length, roomD.width);
+		
+		this.cost = cost;
+	}
+	
+	public double getTotalCost() {
+		return cost * roomD.getArea();
+		
+	}
+	
+	public String toString() {
+		return "" + getTotalCost();
+	}
 	
 	private static class RoomDimension {
 		
